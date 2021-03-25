db87cc205336aa84560dc58833a2e05664fee3f5
alexforsale <alexforsale@yahoo.com>
2021-03-26 02:07:00 +0700

    profile.d: add go.sh

---
+++ .config/profile.d/99-go.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# ~/.config/profile.d/99-go.sh
+# $GOPATH configuration
+# <alexforsale@yahoo.com>
+if [ "$(command -v go)" ];then
+    # set GOPATH to ~/.local so we don't need
+    # to add more PATH
+    export GOPATH=${GOPATH:-~/.local}
+fi
