08415ac0b13b39e402224cc83f30935342051e76
alexforsale <alexforsale@yahoo.com>
2021-03-29 23:29:47 +0700

    set _WM based on ${DESKTOP_SESSION}

--- .config/profile.d/99-window_manager.sh
+++ .config/profile.d/99-window_manager.sh
@@ -4,15 +4,25 @@
 # <alexforsale@yahoo.com>
 
 if [ -z "${_WM}" ]; then
-    if [ "$(command -v openbox-session)" ];then
-        export _WM=openbox
-    elif [ "$(command -v i3)" ];then
-        export _WM=i3
-    elif [ "$(command -v mate-session)" ];then
-        export _WM=mate
-    elif [ "$(command -v xfce4-session)" ];then
-        export _WM=mate
-    elif [ "$(command -v awesome)" ];then
-        export _WM=awesome
-    fi
+    case "${DESKTOP_SESSION}" in
+        "i3"|"i3-with-shmlog")
+            export _WM=i3
+            ;;
+        "openbox")
+            export _WM=openbox
+            ;;
+        *)
+            if [ "$(command -v openbox-session)" ];then
+                export _WM=openbox
+            elif [ "$(command -v i3)" ];then
+                export _WM=i3
+            elif [ "$(command -v mate-session)" ];then
+                export _WM=mate
+            elif [ "$(command -v xfce4-session)" ];then
+                export _WM=mate
+            elif [ "$(command -v awesome)" ];then
+                export _WM=awesome
+            fi
+    ;;
+    esac
 fi
