35153bcf40bc79cbcd4c3f61e6313f3df82bc5d3
alexforsale <alexforsale@yahoo.com>
2022-06-04 01:23:39 +0700

    profile: freebsd addition

--- .config/profile.d/99-freebsd.sh
+++ .config/profile.d/99-freebsd.sh
@@ -5,7 +5,10 @@
 
 if [ "${DISTRO}" = "freebsd" ];then
     ENV=${HOME}/.shrc; export ENV
+    export PAGER=less
     [ -x /usr/bin/resizewin ] && /usr/bin/resizewin -z
     [ -x /usr/bin/fortune ] && /usr/bin/fortune freebsd-tips
+    # Let sh(1) know it's at home, despite /home being a symlink.
+    if [ "$PWD" != "$HOME" ] && [ "$PWD" -ef "$HOME" ] ; then cd ; fi
     cd ${HOME}
 fi
