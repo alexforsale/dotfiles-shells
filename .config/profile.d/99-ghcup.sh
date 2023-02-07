833e7fd649e3a4b3b8e2940fedccd4d0543b3ca9
alexforsale <alexforsale@yahoo.com>
2023-02-07 22:15:56 +0700

    fix ghcup/env

--- .config/profile.d/99-ghcup.sh
+++ .config/profile.d/99-ghcup.sh
@@ -3,8 +3,4 @@
 # haskell's ghcup configuration
 # <alexforsale@yahoo.com>
 
-if [ "$(command -v ghcup)" ];then
-   # set GOPATH to ~/.local so we don't need
-   # to add more PATH
-   [ -f "/home/alexforsale/.ghcup/env" ] && source "/home/alexforsale/.ghcup/env" # ghcup-env
-fi
+[ -f "/home/alexforsale/.ghcup/env" ] && source "/home/alexforsale/.ghcup/env" # ghcup-env
