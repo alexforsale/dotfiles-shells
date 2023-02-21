63e24aa1e02a72b8f311ecb425acc1d491ccf54a
alexforsale <alexforsale@yahoo.com>
2023-02-21 10:11:06 +0700

    add python

---
+++ .config/profile.d/99-python.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# ~/.config/profile.d/99-python.sh
+# Python configuration
+# <alexforsale@yahoo.com>
+
+if [ $(command -v pipenv) ]; then
+    [ -z "${PIPENV_VENV_IN_PROJECT}" ] &&
+        export PIPENV_VENV_IN_PROJECT=1
+fi
