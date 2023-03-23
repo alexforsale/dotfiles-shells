da0510d8650815db304da8a422275a0852010eb3
alexforsale <alexforsale@yahoo.com>
2023-03-23 19:36:44 +0700

    guix: update path

--- .config/profile.d/99-guix.sh
+++ .config/profile.d/99-guix.sh
@@ -11,6 +11,12 @@ if [ -n "${GUIX_LOCPATH}" ];then
    # set the GUIX_BUILD_OPTIONS
    contains "${GUIX_BUILD_OPTIONS}" "-v 3 -c 2 -M 2" ||
        export GUIX_BUILD_OPTIONS="${GUIX_BUILD_OPTIONS} -v 3 -c 2 -M 2"
+   if [ -d "${HOME}/.guix-profile/bin" ]; then
+       export PATH="${HOME}/.guix-profile/bin:${PATH}"
+   fi
+   if [ -d "${HOME}/.guix-profile/sbin" ]; then
+       export PATH="${HOME}/.guix-profile/sbin:${PATH}"
+   fi
 fi
 
 if [ -d "${HOME}/.guix-profile/share/emacs/site-lisp" ]; then
@@ -19,4 +25,3 @@ if [ -d "${HOME}/.guix-profile/share/emacs/site-lisp" ]; then
     contains "${XDG_DATA_DIRS}" "${HOME}/.guix-profile/share" ||
       export XDG_DATA_DIRS="${HOME}/.guix-profile/share:${XDG_DATA_DIRS}"
 fi
-
