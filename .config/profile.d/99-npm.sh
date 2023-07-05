89351dd3b13b6eb604e8cca0f8115a95a15b1166
alexforsale <alexforsale@yahoo.com>
2023-07-05 22:23:44 +0700

    add nvm to npm

--- .config/profile.d/99-npm.sh
+++ .config/profile.d/99-npm.sh
@@ -7,3 +7,10 @@
 if [ "$(command -v npm)" ];then
    export npm_config_prefix="${HOME}/.local"
 fi
+
+if [ -d "${HOME}/.config/nvm" ]; then
+    export NVM_DIR="${HOME}/.config/nvm"
+    [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
+    [ -n "${BASH}" ] &&
+        [ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
+fi
